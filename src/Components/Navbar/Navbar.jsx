1be4746edf353bcb24766073b7433234f90d2c03
Sirius <imc.tapasdash@gmail.com>
2026-02-10 21:54:54 +0530

    Navbar tab design.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,24 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav>
-      <i class="fa-regular fa-house"></i>
+      <div className="tab-wrap">
+        <div className="tab">
+          <i className="fa-regular fa-house"></i>
+          <span>Home</span>
+        </div>
+        <div className="tab">
+          <i className="fa-solid fa-ticket"></i>
+          <span>Bookings</span>
+        </div>
+        <div className="tab">
+          <i className="fa-solid fa-cart-shopping"></i>
+          <span>Cart</span>
+        </div>
+        <div className="tab">
+          <i className="fa-regular fa-user"></i>
+          <span>Me</span>
+        </div>
+      </div>
     </nav>
   )
 }
